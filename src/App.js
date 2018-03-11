0a2e774a6c65c4698168172579acad8fe619c1fc
Preet Dha <manpreetdha+github@gmail.com>
2018-03-10 23:35:02 -0800

    Configure firebase locally with settings derived from firebase web console

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import React from "react";
-import { View, Text } from "react-native";
-import { Header } from "./components/common";
+import React from 'react';
+import { View, Text } from 'react-native';
+import firebase from 'firebase';
+import { Header } from './components/common';
 
 class App extends React.Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyA9vhoQ9TWZ5zUNscp8ZPOTqiItXKVnBJE',
+      authDomain: 'authentication-9f550.firebaseapp.com',
+      databaseURL: 'https://authentication-9f550.firebaseio.com',
+      projectId: 'authentication-9f550',
+      storageBucket: 'authentication-9f550.appspot.com',
+      messagingSenderId: '400999176963'
+    });
+  }
+
   render() {
     return (
       <View>
