457cdba18055fc58f6215beb91f762e8083c2ebe
Preet Dha <manpreetdha+github@gmail.com>
2018-03-20 16:32:33 -0700

    Import ActivityIndicator for Spinner component, and add styles to it

--- src/components/common/Spinner.js
+++ src/components/common/Spinner.js
@@ -1,8 +1,20 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, ActivityIndicator } from 'react-native';
 
-const Spinner = () => {
-  return <View />;
+const Spinner = ({ spinnerSize }) => {
+  return (
+    <View style={styles.spinnerStyle}>
+      <ActivityIndicator size={spinnerSize || 'large'} />
+    </View>
+  );
+};
+
+const styles = {
+  spinnerStyle: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
 };
 
 export { Spinner };
