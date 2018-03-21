9d8890bd3818001d14bb742a2c946f3fff3efcd2
Preet Dha <manpreetdha+github@gmail.com>
2018-03-20 19:03:43 -0700

    Refactor renderContent function into a switch statement, add styling to allow logout button to appear, and add setState call into firebase.auth method call

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './components/common';
+import { Header, Button, CardSection, Spinner } from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends React.Component {
+  state = { loggedIn: null };
+
   componentWillMount() {
     firebase.initializeApp({
       apiKey: 'AIzaSyA9vhoQ9TWZ5zUNscp8ZPOTqiItXKVnBJE',
@@ -14,16 +16,54 @@ class App extends React.Component {
       storageBucket: 'authentication-9f550.appspot.com',
       messagingSenderId: '400999176963'
     });
+
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
+  }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+          <View style={styles.buttonStyles}>
+            <Button buttonText="Log Out" />;
+          </View>
+        );
+
+      case false:
+        return <LoginForm />;
+      default:
+        return (
+          <View style={styles.spinnerStyles}>
+            <Spinner size="large" />;
+          </View>
+        );
+    }
   }
 
   render() {
     return (
       <View>
         <Header headerText="Authentication" />
-        <LoginForm />
+        <View>{this.renderContent()}</View>
       </View>
     );
   }
 }
+
+const styles = {
+  buttonStyles: {
+    flexDirection: 'row',
+    marginTop: 45
+  },
+  spinnerStyles: {
+    marginTop: 55
+  }
+};
 
 export default App;
